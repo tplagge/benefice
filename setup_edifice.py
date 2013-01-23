ebff4ea51b2cddb4fae1a74e2b768e5d2562b23f
Derek Eder <derek.eder@gmail.com>
2013-01-22 21:20:50 -0600

    implemented import_zip function, using OptionParser to pass in --create flags

--- setup_edifice.py
+++ setup_edifice.py
@@ -1,28 +1,47 @@
 # coding: utf-8
+from optparse import OptionParser
 import os
+import glob
 
-def import_zip (file_name, url):
-  os.system("wget -O %s %s" % (file_name, url))
-  os.system("unzip %s" % (file_name,))
-  print "finding shp file in folder"
-  # os.system("shp2pgsql -s 3435 -g the_geom -I | psql -d edifice -f %s" % (file_name,))
-
+def import_zip (url, encoding):
+  os.chdir("import")
+  os.system("wget -O shapefile.zip %s" % (url,))
+  print "unziping..."
+  os.system("unzip shapefile.zip")
   
+  shapefile = ''
+  for f in glob.glob("*.shp"):
+      shapefile = f
+      print 'importing ', shapefile
 
-# os.system("dropdb edifice")
-# os.system("createdb -T base_postgis edifice")
-# os.system("psql -d edifice -f sql_init_scripts/pins_master.sql")
-# os.system("psql -d edifice -f sql_init_scripts/assessed.sql")
+  os.system("shp2pgsql -d -s 3435 -W LATIN1 -g the_geom -I %s | psql -d edifice" % (shapefile,))
+  os.system("rm *")
+  os.chdir("../")
 
-# if os.path.exists("import/pins.dump"):
-#   print "pins.dump exists"
-# else:
-#   os.system("curl -o import/pins.dump http://dl.dropbox.com/u/14915791/pins.dump")
 
-# print "loading property pins"
-# os.system("pg_restore -O -c -d edifice import/pins.dump")
+parser = OptionParser()
+parser.add_option("-c", "--create", default=False, action="store_false",
+                  help="drop existing edifice database and create from scratch")
+
+(options, args) = parser.parse_args()
+
+
+if options.create :
+  os.system("dropdb edifice")
+  os.system("createdb -T base_postgis edifice")
+  os.system("psql -d edifice -f sql_init_scripts/pins_master.sql")
+  os.system("psql -d edifice -f sql_init_scripts/assessed.sql")
+
+  if os.path.exists("import/pins.dump"):
+    print "pins.dump exists"
+  else:
+    os.system("curl -o import/pins.dump http://dl.dropbox.com/u/14915791/pins.dump")
+
+  print "loading property pins"
+  os.system("pg_restore -O -c -d edifice import/pins.dump")
+
 
 print "pulling datasets from data.cityofchicago.org"
 
 print "importing census blocks"
-import_zip("CensusBlocks2010.zip", "https://data.cityofchicago.org/download/3jmu-7ijz/application/zip")
+import_zip("https://data.cityofchicago.org/download/3jmu-7ijz/application/zip", 'LATIN1')
