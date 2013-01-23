b19646bc9e23bda60667ed2aeebe9fdc3eb17db0
Derek Eder <derek.eder@gmail.com>
2013-01-23 01:08:38 -0600

    added 11 more datasets - done with buildings

--- datasets.py
+++ datasets.py
@@ -1,8 +1,11 @@
 # format [name, domain, data_type, socrata_id, options]
 
 datasets = [
+  # buildings
   ['Building Footprints','Chicago','shp','w2v3-isjw','LATIN1'],
   ['Boundaries - Curb Lines','Chicago','shp','5gv8-ktcg','LATIN1'],
+  
+  # boundaries
   ['2010 Census Blocks','Chicago','shp','3jmu-7ijz','LATIN1'],
   ['2010 Census Tracts','Chicago','shp','biqm-wjk3','LATIN1'],
   ['Central Business District','Chicago','shp','uagp-hcv5',''],
@@ -14,5 +17,16 @@ datasets = [
   ['Enterprise Zones','Chicago','shp','v3uj-hd3x',''],
   ['IL House 2000','Chicago','shp','gpzv-tfuc',''],
   ['IL Senate 2000','Chicago','shp','3zsw-bmta',''],
-  ['Industrial Corridors','Chicago','shp','vdsr-p25b','']
+  ['Industrial Corridors','Chicago','shp','vdsr-p25b',''],
+  ['planning_district','Chicago','shp','5xt8-wz7f',''],
+  ['planning_region','Chicago','shp','tcpk-wbgm',''],
+  ['police_districts','Chicago','shp','4dt9-88ua',''],
+  ['precincts','Chicago','shp','sgsc-bb4n',''],
+  ['snow_parking','Chicago','shp','i9q6-fx2v',''],
+  ['special_service_area','Chicago','shp','dbvs-iaa4',''],
+  ['sweeping','Chicago','shp','9rhq-32up',''],
+  ['wards 2001','Chicago','shp','bhcv-wqkf',''],
+  ['winterovernightparkingrestrictions','Chicago','shp','kpim-yiyf',''],
+  ['zip_codes','Chicago','shp','2ka6-iycf',''],
+  ['zoning_aug2012','Chicago','shp','p8va-airx',''],
 ]
