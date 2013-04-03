24397e150e020d33410cc4501f1e8d97ad650cd6
Thomas Plagge <tplagge@Thomas-Plagges-MacBook-Pro.local>
2013-04-02 21:51:29 -0500

    New setup module

---
+++ benefice_setup/__init__.py
@@ -0,0 +1,30 @@
+import yaml
+
+BENEFICE_USER       = 'benefice'
+BENEFICE_DB         = 'benefice'
+POSTGRES_BINDIRNAME = None
+POSTGRES_SUPERUSER  = 'postgres'
+POSTGRES_HOST       = 'localhost'
+DELETE_DOWNLOADS    = False
+
+setup_filename      = 'setup.cfg'
+
+config={}
+try:
+  config = yaml.load(open(setup_filename,'r'))
+except IOError:
+  print('Local config file not found, assuming defaults.')
+
+if 'BENEFICE_USER'       in config.keys():
+  BENEFICE_USER           = config['BENEFICE_USER']
+if 'BENEFICE_DB'         in config.keys():
+  BENEFICE_DB             = config['BENEFICE_DB']
+if 'POSTGRES_BINDIRNAME' in config.keys():
+  POSTGRES_BINDIRNAME     = config['POSTGRES_BINDIRNAME']
+if 'POSTGRES_SUPERUSER'  in config.keys():
+  POSTGRES_SUPERUSER      = config['POSTGRES_SUPERUSER']
+if 'POSTGRES_HOST'       in config.keys():
+  POSTGRES_HOST           = config['POSTGRES_HOST']
+if 'DELETE_DOWNLOADS'    in config.keys():
+  DELETE_DOWNLOADS        = config['DELETE_DOWNLOADS']
+
